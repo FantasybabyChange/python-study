23dfbc9d92ab805418040e28dd62fc4a2e5b888a
fantasybaby <625353861@qq.com>
2020-02-16 20:58:27 +0800

    add variable study

--- python-basic-study/variable_study.py
+++ python-basic-study/variable_study.py
@@ -1,8 +1,8 @@
 # 基本的代码结构
 import time as t
 
-print(t.time() )
-print(t.time()/1000/1000)
+print(t.time())
+print(t.time() / 1000 / 1000)
 # 一行代码换行的话需要 一个tab=四个空格
-if 10-9 > 0:
+if 10 - 9 > 0:
     print('bigger')
