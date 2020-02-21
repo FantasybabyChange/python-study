b68ffc878903acebbd613500e8a404ff504d22f3
liuxi <625353861@qq.com>
2020-02-21 15:20:41 +0800

    add study file

--- python-file-study/file_seek_read.py
+++ python-file-study/file_seek_read.py
@@ -7,6 +7,8 @@ print(file3.read(2))
 file3.seek(2, 0)
 print(file3.tell())
 print(file3.read(2))
+for line in file3.readlines():
+    print(line + "--")
 # print(file3.read(2))
 # print(file3.read(2))
 file3.close()
