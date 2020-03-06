20ef09e30dd81f399b8ec0132d6c388b8b705f6e
liuxi <625353861@qq.com>
2020-03-06 18:48:34 +0800

    add flask file save

--- python-flask-study/app.py
+++ python-flask-study/app.py
@@ -1,18 +1,32 @@
 # -*- coding:utf-8 -*-   # 这行声明python源文件编码，编码信息会被解释器用于解析源文件
-from flask import Flask,request,Response,jsonify
+from flask import Flask, request, Response, jsonify
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/hello', methods=['POST'])
 def hello():
-    print(request.get_json())
-    print(request.args["a"])
+    # if request.is_json:
+    #     script_str = str(request.get_json())
+    # else:
+    script_str = str(request.get_data(), encoding="utf8")
+    if script_str is None:
+        return 'empty data script send failed'  # 啥都没有
+    print(script_str)
+    script_save_path = "/home/fantasybaby/script"
 
-    return "hello world"
+    file_full_path = script_save_path + '/script.txt'
+    if not os.path.exists(script_save_path):
+        os.makedirs(script_save_path)
+        print('create dir %s success' % script_save_path)
+    with open(file_full_path, 'w') as script_file:
+        script_file.write(script_str)
+
+    return ' send success!'  # 返回保存成功的信息
 
 
-print (__name__)
+print(__name__)
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=9000)
     # 会调用werkzeug.serving.run_simple进入轮询
