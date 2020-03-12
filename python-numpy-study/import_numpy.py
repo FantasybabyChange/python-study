e06da0d638354d680593fa0c5a9b5c63319d825e
fantasybaby <625353861@qq.com>
2020-03-12 18:15:26 +0800

    add numpy test

---
+++ python-numpy-study/import_numpy.py
@@ -0,0 +1,16 @@
+# numpy
+import numpy as np
+
+array = np.array([2, 3, 4])
+print(array)
+print(array.dtype)
+
+array1 = np.array([1.2, 1.3, 1.4])
+print(array1)
+print(array1.dtype)
+
+print(array + array1)
+
+print(array * 10)
+
+
