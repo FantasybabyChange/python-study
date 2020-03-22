29ef49433c37946ca39eef06b7ddc21054db002a
fantasybaby <625353861@qq.com>
2020-03-23 00:00:54 +0800

    add ip

--- python-numpy-study/import_numpy.py
+++ python-numpy-study/import_numpy.py
@@ -13,4 +13,22 @@ print(array + array1)
 
 print(array * 10)
 
+zero_array = np.zeros(10)
+print(zero_array)
 
+zero_array = np.zeros((3, 5))
+print(zero_array)
+
+ones = np.ones(3)
+print(ones)
+
+empty = np.empty((3, 2, 3))
+print(empty)
+
+arange = np.arange(10)
+
+print(arange[2:7])
+
+arange_copy = arange[7:10].copy()
+arange_copy[:] = 1
+print(arange_copy)
