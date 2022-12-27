fb4ca2466ccd9ebacfcc54bf8759533cc941e834
FantasybabyChange <625353861@qq.com>
2022-12-27 11:20:41 +0800

    add pythoon

--- python-file-study/file_seek_read.py
+++ python-file-study/file_seek_read.py
@@ -1,14 +1,20 @@
 import os
-file3 = open("name.txt")
-size = os.path.getsize("name.txt")
+file3 = open("txt.txt")
+size = os.path.getsize("txt.txt")
+inputValue = input("please input:\n")
 print(size)
-
+print(inputValue)
 print(file3.read(2))
 file3.seek(2, 0)
 print(file3.tell())
 print(file3.read(2))
 for line in file3.readlines():
-    print(line + "--")
+    if line.find(inputValue) >= 0:
+        print("yes ")
+    else:
+        print("不存在")
+
+
 # print(file3.read(2))
 # print(file3.read(2))
 file3.close()
